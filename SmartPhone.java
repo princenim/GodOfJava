3439b52acd9d8dea373739afd29e6ecae91d22cd
hazel <waithjno@gmail.com>
2024-01-01 17:30:06 +0900

    update 3장

---
+++ SmartPhone.java
@@ -0,0 +1,24 @@
+/**
+ * @author hazel
+ */
+public class SmartPhone {
+    boolean power;
+
+    boolean call;
+
+    boolean text;
+
+    public void turnOnPower(){
+
+    }
+
+     public  void calling(){
+        //전화걸기
+
+    }
+
+    public void texting(){
+        //문자 보내기
+    }
+
+}
