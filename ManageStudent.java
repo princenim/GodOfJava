0d9dc6f5d2dbb4d957985c116f4ee0e3736c49a6
hazel <waithjno@gmail.com>
2024-01-04 22:47:10 +0900

    update 12장

--- ManageStudent.java
+++ ManageStudent.java
@@ -5,8 +5,10 @@ public class ManageStudent {
     public static void main(String[] args) {
         Student[] student = null;
         ManageStudent manageStudent = new ManageStudent();
-        student = manageStudent.addStudent();
-        manageStudent.printStudents(student);
+        //student = manageStudent.addStudent();
+        //manageStudent.printStudents(student);
+
+        manageStudent.checkEquals();
 
 
     }
@@ -24,6 +26,21 @@ public class ManageStudent {
             System.out.println(data);
 
         }
+
+    }
+
+    public void checkEquals() {
+        //값이 같은 두 객체 생성
+        Student a = new Student("Min", "HI", "0000", "AA");
+        Student b = new Student("Min", "HI", "0000", "AA");
+
+        //객체가 같으면 -> 객체의 값이 같은지 확인하려면 equals가 오버라이드 된 상태로 equal를 사용해야함. 단순 주소 비교는 == 로 가능!
+        if (a.equals(b)) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not Equal");
+        }
+
 
     }
 }
--- Student.java
+++ Student.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 /**
  * @author hazel
  */
@@ -7,19 +9,35 @@ public class Student {
     String phone;
     String email;
 
-    //생성자
-    Student(String name){
+    Student(String name) { //생성자
         this.name = name;
     }
 
-    Student(String name, String address, String phone, String email){
+    Student(String name, String address, String phone, String email) { ////생성자
         this.name = name;
         this.address = address;
         this.phone = phone;
         this.email = email;
     }
 
-    public String toString(){
-        return name+" "+address+" "+phone+" "+email;
+
+    public String toString() { //Object 클래스 상속
+        return name + " " + address + " " + phone + " " + email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name)   //인스턴스 변수 name과 객체로 넘어온 o의 객체 name을 비교, 위에서 형변환 했음.
+                && Objects.equals(address, student.address)
+                && Objects.equals(phone, student.phone)
+                && Objects.equals(email, student.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, address, phone, email);
     }
 }
